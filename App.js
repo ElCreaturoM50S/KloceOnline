f432d7f5288310a3b466b9aef3d14209f976a1df
ElCreaturoM50S <113674920+ElCreaturoM50S@users.noreply.github.com>
2023-03-31 09:39:20 +0200

    start working on login bar

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { useEffect, useState, useCallback} from 'react'
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import {
+   createMaterialTopTabNavigator 
+} from '@react-navigation/material-top-tabs';
 
-export default function App() {
+
+function Login() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <form>
+        <>
+          <label>Login</label>
+          <input type="text" placeholder="Login"/>
+        </>
+        <br/>
+        <>
+          <label>Password</label>
+          <input type="text" placeholder="Password"/>
+        </> 
+        <br/>
+        <>
+          <label>Nickname</label>
+          <input type="text" placeholder="Password"/>
+        </> 
+        <br/> 
+        <>
+          <label>Email</label>
+          <input type="text" placeholder="Password"/>
+        </>  
+      </form>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Kloce() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications Screen</Text>
+    </View>
+  );
+}
+
+function Zegar() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Ratio</Text>
+    </View>
+  )
+}
+
+const Tab = createMaterialTopTabNavigator()
+
+function MyTabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Login" component={Login} />
+      <Tab.Screen name="Kloce" component={Kloce} />
+      <Tab.Screen name= "Zegar" component={Zegar} />
+    </Tab.Navigator>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabNavigator />
+    </NavigationContainer>
+  )
+}
